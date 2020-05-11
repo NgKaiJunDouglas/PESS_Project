192ca3128bb139a0f2ac530234ff0a070771f84e
NgKaiJunDouglas <douglas10pvp@gmail.com>
2020-05-11 19:53:59 +0800

    Create nav.php

---
+++ 17_Ng_Kai_Jun_pessv1/nav.php
@@ -0,0 +1,7 @@
+<div id="banner" ><img src="Banner.png" border="1"></div>
+<div class="topnav" >
+<a href="logcall.php"><span>Log Call</span></a>
+<a href="update.php">Update</a>
+<a href="#">Report</a>
+<a href="#">History</a>
+</div>
